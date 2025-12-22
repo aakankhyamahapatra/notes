cf5a235ca75093bafa734c7fa18c3441b0e6d68c
Sruti <mahapatraaakankhya7@gmail.com>
2025-12-22 21:47:36 +0530

    add Python overview notes

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 # contents to be covered:
 # basic principles, control flow, advanced datatypes, functional programming, OOP, data aquisition, 3rd party libraries
+
+# 1. created by Guido van Rossum in 1989 while working at 
+
+# 2. Developement :
+# python 2.0 released in 2000 :- last released was 2.7
+# python 3.0 released in 2008 :- last released 3.9
+
+# 3. Defination :- Python is a dynamically typed, general purpose programming language that supports an OOPs approach as well as a functional programming approach.
+
+# 4. Implementations of python are : CPython & PyPy
+
+# 5. The reference Python implementation is CPython
+
+# 6. A little more about CPython 
+# {Reference implementaion :- http://www.python.org
+# It is open source written in C 
+# Includes the standard library
+# A collection of additional functionality that goes beyond just the python language}
